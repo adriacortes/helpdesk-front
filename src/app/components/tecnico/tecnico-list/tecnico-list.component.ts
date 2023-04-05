2f35f2cc675b571dd677293313d39279787c5ad8
Ádria Aline Castro da Silva Côrtes <adria.aline@gmail.com>
2023-04-05 16:24:22 -0300

    Html e Css para Tecnico List

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
   templateUrl: './tecnico-list.component.html',
   styleUrls: ['./tecnico-list.component.css']
 })
-export class TecnicoListComponent {
+export class TecnicoListComponent implements OnInit {
+
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id:1,
+      nome:'Adria Aline',
+      cpf:'03312168171',
+      email:'adria.aline@gmail.com',
+      senha:'123',
+      perfis:['0'],
+      dataCriacao:'05/04/2023'
+    }
+  ];
+  
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
 }
+
