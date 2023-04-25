b447fe9865686ac376c6c3e4e323c173315c3746
Ádria Aline Castro da Silva Côrtes <adria.aline@gmail.com>
2023-04-25 11:41:17 -0300

    Subindo projeto para o GitHub

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,3 +1,4 @@
+import { DataSource } from '@angular/cdk/collections';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
@@ -21,16 +22,26 @@ export class TecnicoListComponent implements OnInit {
       senha:'123',
       perfis:['0'],
       dataCriacao:'05/04/2023'
+    },
+    {
+      id:2,
+      nome:'Luana Castro',
+      cpf:'03312168171',
+      email:'luana@gmail.com',
+      senha:'123',
+      perfis:['0'],
+      dataCriacao:'05/04/2023'
     }
   ];
   
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','acoes'];
   dataSource = new MatTableDataSource<Tecnico>(this.list);
-
+  
   constructor(private service:TecnicoService){}
   
   ngOnInit(): void {
     this.findAll();
+
   }
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -43,9 +54,12 @@ export class TecnicoListComponent implements OnInit {
     this.service.findAll().subscribe((resposta) =>{
       resposta.forEach(tecnicos => {
         this.list.push(tecnicos);
+        debugger
       })
     })
   }
+
+  
 
 }
 
